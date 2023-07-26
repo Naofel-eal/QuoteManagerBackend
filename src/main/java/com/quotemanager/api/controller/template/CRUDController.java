9de9c310cf390fb5cf110d378fd4c47c9268d15d
Naofel EL ALOUANI <naofel.eal@gmail.com>
2023-07-26 18:07:40 +0200

    CRUDController template

--- src/main/java/com/quotemanager/api/controller/template/CRUDController.java
+++ src/main/java/com/quotemanager/api/controller/template/CRUDController.java
@@ -1,10 +1,13 @@
 package com.quotemanager.api.controller.template;
 
 import com.quotemanager.api.service.template.CRUDService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Collection;
 
-@RestController
 public class CRUDController<ENTITY, SERVICE extends CRUDService<ENTITY>> {
 
     private SERVICE service;
@@ -13,19 +16,33 @@ public class CRUDController<ENTITY, SERVICE extends CRUDService<ENTITY>> {
         this.service = service;
     }
 
-    @GetMapping("/healthz")
-    public ResponseEntity healthz() {
-        return ResponseEntity.ok().build();
-    }
-
     @PostMapping("/create")
-    public ResponseEntity create(@RequestBody ENTITY entity) {
-        this.service.create(entity);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ENTITY> create(@Valid @RequestBody ENTITY entity) {
+        ENTITY resultEntity = this.service.create(entity);
+        return new ResponseEntity<>(resultEntity, HttpStatus.CREATED);
     }
 
-    @GetMapping("/get")
-    public ResponseEntity get(@RequestBody int id) {
-        return this.s
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ENTITY> get(@PathVariable Long id) {
+        ENTITY entity = this.service.get(id);
+        return entity != null ? ResponseEntity.ok(entity) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/list/{limit}")
+    public ResponseEntity<Collection<ENTITY>> list(@PathVariable int limit) {
+        Collection<ENTITY> entities = this.service.list(limit);
+        return new ResponseEntity<>(entities, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ENTITY> update(@Valid @RequestBody ENTITY entity) {
+        ENTITY resultEntity = this.service.update(entity);
+        return new ResponseEntity<>(resultEntity, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
